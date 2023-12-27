a2bd2cdff90d27d9fb7bbc5f7082133e96767370
chibaf <chibaf@mac.com>
2023-12-27 23:52:57 +0900

    Add files via upload

--- plotcsv.py
+++ plotcsv.py
@@ -2,15 +2,18 @@ import csv
 import sys
 import matplotlib.pyplot as plt
 
+# read csv file(sys.argv[1])
 a=[]
 with open(sys.argv[1]) as csvfile:
   inputcsv=csv.reader(csvfile, delimiter=',', quotechar='|')
   for row in inputcsv:
     a.append(row)
-    
+
+# transpose matrix and remove the first row   
 a=list(map(list,(zip(*a))))
 a=a[1:len(a)-1]
 
+# plot data
 x=range(0,len(a[1]))
 plt.clf()
 plt.ylim(-10,30)
