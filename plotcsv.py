0b6113f30cf74715e45f53b4c1095af82dd8833a
chibaf <chibaf@mac.com>
2023-12-27 23:50:22 +0900

    Add files via upload

--- plotcsv.py
+++ plotcsv.py
@@ -1,13 +1,19 @@
 import csv
 import sys
+import matplotlib.pyplot as plt
 
 a=[]
 with open(sys.argv[1]) as csvfile:
-  inputcsv = csv.reader(csvfile, delimiter=',', quotechar='|')
+  inputcsv=csv.reader(csvfile, delimiter=',', quotechar='|')
   for row in inputcsv:
     a.append(row)
     
-a=list(map(list, (zip(*a))))
-#print(a[1])
-a=a[1:len(a)]
-print(a)
+a=list(map(list,(zip(*a))))
+a=a[1:len(a)-1]
+
+x=range(0,len(a[1]))
+plt.clf()
+plt.ylim(-10,30)
+for i in range(1,len(a)-1):
+  plt.plot(x,a[i])
+plt.show()
